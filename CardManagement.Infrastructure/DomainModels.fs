2c71e3d90718b371aae68c2a81b16033f47370f2
Jermuck <k.bardyugov@yandex.ru>
2023-11-17 21:15:51 +0500

    add Models In Infrastructure

--- CardManagement.Infrastructure/DomainModels.fs
+++ CardManagement.Infrastructure/DomainModels.fs
@@ -1,9 +1,46 @@
 namespace CardManagement.Infrastructure
 
-module DomainModels = 
+open System
+
+module DomainModels =
+    
+    type TypeOfActivation =
+        | Activate
+        | Deactivate
+        
+    type TypeOfCard =
+        | Basic
+        | Priority
+        | Credit
+        
+    
+    type Transaction = {
+        Id: string
+        Card: Card
+        CreateDate: DateTime
+        Sum: int
+        To: string
+    }
+        
+    and Card = {
+        Id: string
+        Code: int
+        CVV: int
+        UserId: string
+        TypeCard: TypeOfCard
+        Balance: int
+        Transactions: Transaction option
+        LifeTime: DateTime
+        isActive: TypeOfActivation
+    }
 
     type User = {
-        Id: int
+        Id: string
         Name: string
         Surname: string
+        Patronymic: string
+        Age: int
+        Salary: int
+        Email: string
+        Cards: Card[] option
     }
--- Console/Program.fs
+++ Console/Program.fs
@@ -2,10 +2,4 @@
 
 [<EntryPoint>]
 let main _ =
-    let user = {
-        Id = 1
-        Name = ""
-        Surname = "" 
-    }
-    printfn "%A" user
     0
