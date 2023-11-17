840bbf8e02c125f6253969f73f130ed30509ca61
Jermuck <k.bardyugov@yandex.ru>
2023-11-17 14:11:54 +0500

    Init

---
+++ CardManagement.Infrastructure/DomainModels.fs
@@ -0,0 +1,9 @@
+namespace CardManagement.Infrastructure
+
+module DomainModels = 
+
+    type User = {
+        Id: int
+        Name: string
+        Surname: string
+    }
---
+++ Console/Program.fs
@@ -0,0 +1,11 @@
+﻿open CardManagement.Infrastructure.DomainModels
+
+[<EntryPoint>]
+let main _ =
+    let user = {
+        Id = 1
+        Name = ""
+        Surname = "" 
+    }
+    printfn "%A" user
+    0
