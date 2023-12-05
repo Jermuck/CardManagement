caf7aed8c95a7e22d9a6eacf90159e5c26270fe0
Jermuck <k.bardyugov@yandex.ru>
2023-12-05 22:54:42 +0500

    update .fsproj and add MakeFile

--- CardManagement.Data/Settings.fs
+++ CardManagement.Data/Settings.fs
@@ -1,15 +1,5 @@
 namespace CardManagement.Data
 
-open Microsoft.Extensions.Configuration
-
 module Settings =
-    let configService = ConfigurationBuilder()
-                            .SetBasePath("./Properties")
-                            .AddJsonFile("appsettings.json", true, true)
-                            .Build()
-    
-    let private pathSectionDatabase = "AppSettings:Database:"
-    
-    let username = configService.GetSection(pathSectionDatabase + "Name").Value
-    let password = configService.GetSection(pathSectionDatabase + "Password").Value
-    
+    let 
+
