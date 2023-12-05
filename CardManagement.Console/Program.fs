d0dda24b6ddaca6ed4e78570ee20ba01c25d9a62
Jermuck <k.bardyugov@yandex.ru>
2023-12-05 22:25:37 +0500

    Update CardManagement.Data

--- CardManagement.Console/Program.fs
+++ CardManagement.Console/Program.fs
@@ -1,20 +1,7 @@
-open System
-open System.Threading.Tasks
-open CardManagement.Infrastructure.DomainModels
-open CardManagement.Infrastructure.CardActions
-open CardManagement.Data.DatabaseConfiguration
+open CardManagement.Data.Repository
+
 [<EntryPoint>]
 let main _ =
-    let mockUser: User = {
-       Id = Guid.NewGuid()
-       Name = "John"
-       Surname = "Doe"
-       Patronymic = "Smith"
-       Age = 30
-       Salary = 50000
-       Email = "john.doe@example.com"
-    }
-    let card = buildCard mockUser Basic 100
-    let users = selectUsers() |> Task.WaitAll
-    printfn "%A" users
+    let t = test() |> Async.AwaitTask |> Async.RunSynchronously
+    printfn "%A" t
     0 
--- CardManagement.Data/Repository.fs
+++ CardManagement.Data/Repository.fs
@@ -1,10 +1,22 @@
 namespace CardManagement.Data
 
-module Repository = 
 
+module Repository =
+    open SqlHydra.Query
+    open CardManagement.Data.``public``
+    open Npgsql
+    
     let openContext() = 
-
-        let compiler = SqlKata.Compilers.SqlServerCompiler()
-        let conn = new SqlConnection("Replace with your connection string")
-        conn.Open()
+        let compiler = SqlKata.Compilers.PostgresCompiler()
+        let conn = new NpgsqlConnection("server=localhost; database=cardmanagement; username=root; password=12345")
+        conn.Open() |> ignore
         new QueryContext(conn, compiler)
+    
+    let test() =
+        insertTask (Create openContext) {
+            for p in customers do
+                entity {
+                    customer_id = 4
+                    customer_name = "dasda"
+                }
+        }
