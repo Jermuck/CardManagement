d9c948e7ea4b28cf075e284fe4f56317c18a02ef
Jermuck <k.bardyugov@yandex.ru>
2023-12-06 18:54:53 +0500

    add Settings

--- CardManagement.Data/Settings.fs
+++ CardManagement.Data/Settings.fs
@@ -1,5 +1,15 @@
 namespace CardManagement.Data
 
-module Settings =
-    let 
+open System.Diagnostics
+open System.Reflection
 
+module Settings =
+    open Microsoft.Extensions.Configuration
+    open System.IO
+    
+    let configService = ConfigurationBuilder()
+                            .SetBasePath("/Users/kirillbardugov/Desktop/Projects/f#/CardManagement/CardManagement.Data/Properties")
+                            .AddJsonFile("appsettings.json", true, true)
+                            .Build()
+    
+    let connectionString = configService.GetSection("AppSettings:ConnectionString").Value
